32d5916d8af9d9ef0e96ac19a50499d68b65602e
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-04-28 19:54:48 -0300

    descomntando serilog

--- Program.cs
+++ Program.cs
@@ -3,36 +3,29 @@ using Serilog;
 using take_note.Domain;
 using take_note.Services;
 
-
-
-// var log = new LoggerConfiguration()
-//     .WriteTo.Console()
-//     .WriteTo.File("serilogs/log.txt", rollingInterval: RollingInterval.Day)
-//     .Filter.With(new CustomLogFilter())
-//     .CreateLogger();
-// log.Information("Inicio da aplicação MXM Sistemas");
-// Log.Logger = log;
+var log = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("serilogs/log.txt", rollingInterval: RollingInterval.Day)
+    .Filter.With(new CustomLogFilter())
+    .CreateLogger();
+log.Information("Inicio da aplicação MXM Sistemas");
+Log.Logger = log;
 
 
 var builder = WebApplication.CreateBuilder(args);
-// builder.Services.AddSerilog(); 
-// builder.Host.UseSerilog(); 
+builder.Services.AddSerilog();
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// builder.Services.AddDbContext<MySqlDbContext>(options =>
-// {
-//   var defaultConnectionString = builder.Configuration.GetConnectionString("MySqlConnection");
-//   options.UseMySql(defaultConnectionString, ServerVersion.AutoDetect(defaultConnectionString))
-//   .EnableSensitiveDataLogging() 
-//   .LogTo(Console.WriteLine, LogLevel.Debug); 
-// });
 builder.Services.AddDbContext<MySqlDbContext>(options =>
 {
   var defaultConnectionString = builder.Configuration.GetConnectionString("RailWayConnection");
-  options.UseMySql(defaultConnectionString, ServerVersion.AutoDetect(defaultConnectionString));
+  options.UseMySql(defaultConnectionString, ServerVersion.AutoDetect(defaultConnectionString))
+   .EnableSensitiveDataLogging()
+   .LogTo(Console.WriteLine, LogLevel.Debug);
 });
 
 builder.Services.AddScoped<INoteService, NoteService>();
